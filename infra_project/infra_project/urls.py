869267579ca299f5d9005d827ceca469ecf63b69
ivanb <ivanbor2016@gmail.com>
2023-03-08 12:32:25 +0300

    1

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from django.contrib import admin
 
-
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
